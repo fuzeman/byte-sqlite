737d748f868e06e83455dd5c3a6c40d740a39942
Dean Gardiner <me@dgardiner.net>
2017-05-15 10:47:42 +1200

    Added basic `insert` test

---
+++ tests/compiler/insert_tests.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, division, print_function
+
+from byte.collection import Collection
+from byte.model import Model
+from byte.property import Property
+
+from hamcrest import *
+
+
+class User(Model):
+    class Options:
+        slots = True
+
+    id = Property(int, primary_key=True)
+
+    username = Property(str)
+    password = Property(str)
+
+
+def test_insert():
+    """Test insert statement is complied correctly."""
+    users = Collection(User, 'sqlite://:memory:?table=users')
+
+    sql, parameters = users.executor.compiler.compile(users.insert(
+        User['username'],
+        User['password']
+    ).items({
+        'username': 'one',
+        'password': 'two'
+    }))
+
+    assert_that(sql, equal_to('INSERT INTO "users" ("users"."username", "users"."password") VALUES (?, ?);'))
+    assert_that(parameters, equal_to(('one', 'two')))
