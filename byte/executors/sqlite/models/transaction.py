9a12d657e6df3c83836e0fbdc0ca82b2d12e7dbb
Dean Gardiner <me@dgardiner.net>
2017-05-17 22:36:31 +1200

    executor: transaction: added messages for debugging transaction actions

--- byte/executors/sqlite/models/transaction.py
+++ byte/executors/sqlite/models/transaction.py
@@ -4,6 +4,10 @@ from __future__ import absolute_import, division, print_function
 from byte.executors.core.models.database import DatabaseTransaction
 from byte.executors.sqlite.models.cursor import SqliteCursor
 
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class SqliteTransaction(DatabaseTransaction, SqliteCursor):
     """SQLite transaction class."""
@@ -12,10 +16,16 @@ class SqliteTransaction(DatabaseTransaction, SqliteCursor):
         """Begin transaction."""
         self.instance.execute('BEGIN;')
 
+        log.debug('BEGIN')
+
     def commit(self):
         """Commit transaction."""
+        log.debug('COMMIT')
+
         self.connection.instance.commit()
 
     def rollback(self):
         """Rollback transaction."""
+        log.debug('ROLLBACK')
+
         self.connection.instance.rollback()
